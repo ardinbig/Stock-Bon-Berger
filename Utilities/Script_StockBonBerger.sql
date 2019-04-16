c07c12259d629159b8acaa032b7fe0d50cfab582
Ardin Biganiro <ardinbig@gmail.com>
2019-04-16 12:17:36 +0200

    Création de tables

--- Utilities/Script_StockBonBerger.sql
+++ Utilities/Script_StockBonBerger.sql
@@ -5,7 +5,7 @@ GO
 USE StockBonBerger_DB
 GO
 
-
+--- CREATION DE TABLES ---
 
 CREATE TABLE tFournisseur
 (
@@ -91,7 +91,7 @@ CREATE TABLE tDetailApprov
 	code_approv UNIQUEIDENTIFIER NOT NULL,
 	code_piece UNIQUEIDENTIFIER NOT NULL,
 	quantite INT NOT NULL,
-	montant DOUBLE,
+	prix DOUBLE,
 		CONSTRAINT fk_detailApprov_approv FOREIGN KEY (code_approv)
 			REFERENCES tApprov(code_guid),
 		CONSTRAINT fk_detailApprov_piece FOREIGN KEY (code_piece)
@@ -118,13 +118,66 @@ CREATE TABLE tDetailCommande
 	code_cmd UNIQUEIDENTIFIER NOT NULL,
 	code_piece UNIQUEIDENTIFIER NOT NULL,
 	quantite INT NOT NULL,
-	montant DOUBLE,
+	prix DOUBLE,
 		CONSTRAINT fk_detailCmd_cmd FOREIGN KEY (code_cmd)
 			REFERENCES tCommande(code_guid),
 		CONSTRAINT fk_detailCmd_piece FOREIGN KEY (code_piece)
 			REFERENCES tPiece(code_guid)
 )
 GO
+
+CREATE TABLE tVente
+(
+	id BIGINT IDENTITY(1,1),
+	code_guid UNIQUEIDENTIFIER PRIMARY KEY, 
+	date_vente DATETIME DEFAULT GETDATE(),
+	code_client UNIQUEIDENTIFIER NOT NULL,
+	agent NVARCHAR(20) NOT NULL,
+		CONSTRAINT fk_vente_client FOREIGN KEY (code_client)
+			REFERENCES tClient(code_guid)
+)
+GO
+
+CREATE TABLE tDetailVente
+(
+	id BIGINT IDENTITY(1,1),
+	code_guid UNIQUEIDENTIFIER PRIMARY KEY, 
+	code_vente UNIQUEIDENTIFIER NOT NULL,
+	code_piece UNIQUEIDENTIFIER NOT NULL,
+	quantite INT NOT NULL,
+	prix DOUBLE,
+		CONSTRAINT fk_detailVente_vente FOREIGN KEY (code_vente)
+			REFERENCES tVente(code_guid),
+		CONSTRAINT fk_detailVente_piece FOREIGN KEY (code_piece)
+			REFERENCES tPiece(code_guid)
+)
+GO
+
+CREATE TABLE tModePaiement
+(
+	id BIGINT IDENTITY(1,1),
+	code_guid UNIQUEIDENTIFIER PRIMARY KEY,
+	designation NVARCHAR(100) NOT NULL,
+	date_maj DATETIME DEFAULT GETDATE(),
+)
+GO
+
+CREATE TABLE tPaiement
+(
+	id BIGINT IDENTITY(1,1),
+	code_guid UNIQUEIDENTIFIER PRIMARY KEY,
+	code_vente UNIQUEIDENTIFIER NOT NULL,
+	mode_paiement UNIQUEIDENTIFIER NOT NULL
+	montant_paye DOUBLE,
+	agent agent NVARCHAR(20) NOT NULL,
+		CONSTRAINT fk_pmt_vente FOREIGN KEY (code_vente)
+			REFERENCES tVente(code_guid),
+		CONSTRAINT fk_pmt_modePmt FOREIGN KEY (mode_paiement)
+			REFERENCES tModePaiement(code_guid)
+)
+GO
+
+
 
 
 
