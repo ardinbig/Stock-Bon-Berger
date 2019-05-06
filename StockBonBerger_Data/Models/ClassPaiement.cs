e8119d9b6447c247a9c8998c5cf4a50b0a9e015d
Ardin Biganiro <ardinbig@gmail.com>
2019-05-06 12:08:04 +0200

    Création de la classe Paiement

---
+++ StockBonBerger_Data/Models/ClassPaiement.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockBonBerger_Data.Models
+{
+    class Paiement : Base
+    {
+        public double MontantPaye { get; set; }
+
+        public string CodeModePaiement { get; set; }
+
+        public string Agent { get; set; }
+    }
+}
