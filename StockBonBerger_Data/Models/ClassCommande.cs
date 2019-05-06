deed1cc51251a4a7b3d0b767546355b003277c52
Ardin Biganiro <ardinbig@gmail.com>
2019-05-06 11:58:09 +0200

    Création de la class Commande

---
+++ StockBonBerger_Data/Models/ClassCommande.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockBonBerger_Data.Models
+{
+    class Commande : Base
+    {
+        public DateTime DateCommande { get; set; }
+
+        public string CodeClient { get; set; }
+
+        public string Agent { get; set; }
+
+        public string Etat { get; set; }
+    }
+}
