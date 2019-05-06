6a238cb0bd2e4113a863365a30910d7ae16718f7
Ardin Biganiro <ardinbig@gmail.com>
2019-05-06 12:13:11 +0200

    Création de la classe Piece

---
+++ StockBonBerger_Data/Models/ClassCategoriePiece.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockBonBerger_Data.Models
+{
+    class CategoriePiece : Base
+    {
+        public string Designation { get; set; }
+    }
+}
---
+++ StockBonBerger_Data/Models/ClassPiece.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockBonBerger_Data.Models
+{
+    class Piece : Base
+    {
+        public string Designation { get; set; }
+
+        public string CodeCategoriePiece { get; set; }
+
+        public string LieuFabrication { get; set; }
+
+        public string Usage { get; set; }
+    }
+}
