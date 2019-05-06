7b2866d5fe5d79c4e53fc31415cf78f68c599138
Ardin Biganiro <ardinbig@gmail.com>
2019-05-06 12:02:02 +0200

    Création de la class Vente

---
+++ StockBonBerger_Data/Models/ClassVente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockBonBerger_Data.Models
+{
+    class Vente : Base
+    {
+        public DateTime DateVente { get; set; }
+
+        public string CodeClient { get; set; }
+
+        public string Agent { get; set; }
+    }
+}
